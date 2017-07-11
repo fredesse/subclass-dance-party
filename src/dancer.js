d722223a69901d9985e537fa820ddd8e286939bd
fredesse <eleegia@gmail.com>
2017-07-10 20:04:39 -0700

    Everything refactored to pseudoclassical

--- src/dancer.js
+++ src/dancer.js
@@ -9,34 +9,31 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
   this.step();
 
-
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
+
   this.setPosition(top, left);
-
-
 };
+
 
 // the basic dancer doesn't do anything interesting at all on each step,
 // it just schedules the next step
 makeDancer.prototype.step = function() {
-  setTimeout(this.step, this.timeBetweenSteps);
+
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
 makeDancer.prototype.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    this.$node.css(styleSettings);
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //this.$node = $('<span class="dancer"></span>');
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+
+  this.$node.css(styleSettings);
 };
-
-
-
-
-
 
 
 
